bc409fa5ad363a3778dc14c89c8fdb21db5bd92e
thefakeblue <122709556+thefakeblue@users.noreply.github.com>
2026-04-15 19:26:27 -0600

    Use 1-minute historical data; update fetch script
    Replace HistoricalData.csv with HistoricalData1Min.csv (switch to 1-minute bars) and update app/fetchdata.hs accordingly. Update trades.csv to reflect changes. Note: this changeset also includes generated GHC/dist-newstyle build artifacts and cache updates produced by compilation.

--- app/fetchdata.hs
+++ app/fetchdata.hs
@@ -4,34 +4,40 @@ import qualified Data.ByteString.Char8 as BS
 import Control.Monad (forever)
 import Text.Read (readMaybe)
 import System.IO
+import Backtest
+import Strategies
+import Data.IORef
 import Data.Time
 import qualified Data.Set as Set
-import Data.IORef
-import System.IO.Error (tryIOError)
-import System.Directory (doesFileExist)
 import Data.Maybe (mapMaybe)
+import System.Directory (doesFileExist)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BS
 
--- MAIN
+
+
 main :: IO ()
 main = withSocketsDo $ do
     addr <- resolve
     sock <- open addr
-    putStrLn "Data logger listening on port 5002..."
+    putStrLn "Server listening on port 5001..." -- 5000 not working for storey
 
-    -- Open CSV
-    handle <- openFile "HistoricalData.csv" AppendMode
 
-    -- Load existing timestamps
-    existingTimes <- loadExistingTimestamps "HistoricalData.csv"
+    existingTimes <- loadExistingTimestamps "HistoricalData1Min.csv"
+
+    -- open CSV file once
+    handle <- openFile "HistoricalData1Min.csv" AppendMode
+
+    stateRef <- newIORef initialBacktestState -- added by storey
+
     timeSetRef <- newIORef existingTimes
 
     (conn, _) <- accept sock
     putStrLn "Client connected"
-    handleClient conn handle timeSetRef
+    handleClient conn handle stateRef timeSetRef
 
--- HANDLE CLIENT
-handleClient :: Socket -> Handle -> IORef (Set.Set UTCTime) -> IO ()
-handleClient conn handle timeSetRef = do
+handleClient :: Socket -> Handle -> IORef BacktestState -> IORef (Set.Set UTCTime) -> IO ()
+handleClient conn handle stateRef timeSetRef = do
     msg <- NBS.recv conn 1024
 
     if BS.null msg
@@ -45,86 +51,71 @@ handleClient conn handle timeSetRef = do
                     putStrLn "Parse error"
                     NBS.sendAll conn (BS.pack "HOLD\n")
 
-                Just (time, o, h, l, c) -> do
+                Just (time, o,h,l,c) -> do 
+                    let marketData = MarketData
+                         { timestamp = time
+                         , openPrice = o
+                         , highPrice = h
+                         , lowPrice = l
+                         , closePrice = c
+                         }
+                        action = "HOLD"
+                            
+                    oldState <- readIORef stateRef
+                    let newState = stepBacktest simpleStrategy oldState marketData
+                    writeIORef stateRef newState
+
+                    putStrLn ("Sending: " ++ action)
+                    putStrLn "Backtest state: "
+                    print newState -- prints updated state
+
+                        -- WRITE TO CSV, needs to be after action
                     existingTimes <- readIORef timeSetRef
 
                     if Set.member time existingTimes
-                        then putStrLn "Duplicate timestamp, skipping..."
+                        then putStrLn "Duplicate timestamp (already in CSV), skipping..."
                         else do
-                            -- Write to CSV
                             hPutStrLn handle $
-                                show time ++ "," ++
+                                formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S" time ++ "," ++
                                 show o ++ "," ++
                                 show h ++ "," ++
                                 show l ++ "," ++
-                                show c
+                                show c ++ "," ++
+                                action
 
                             hFlush handle
 
-                            -- Update set
                             writeIORef timeSetRef (Set.insert time existingTimes)
 
                             putStrLn "Logged new data"
 
-                    -- ALWAYS HOLD
-                    NBS.sendAll conn (BS.pack "HOLD\n")
+                    NBS.sendAll conn (BS.pack (action ++ "\n"))
+            handleClient conn handle stateRef timeSetRef -- loop to handle next message
 
-            handleClient conn handle timeSetRef
-
--- LOAD EXISTING CSV TIMES
-loadExistingTimestamps :: FilePath -> IO (Set.Set UTCTime)
-loadExistingTimestamps path = do
-    exists <- doesFileExistSafe path
-    if not exists
-        then return Set.empty
-        else do
-            content <- readFile path
-            let times = mapMaybe extractTime (lines content)
-            return (Set.fromList times)
-
--- SAFE FILE CHECK
-doesFileExistSafe :: FilePath -> IO Bool
-doesFileExistSafe path = do
-    result <- tryIOError (withFile path ReadMode (\_ -> return ()))
-    case result of
-        Left _  -> return False
-        Right _ -> return True
-
--- EXTRACT TIME FROM CSV LINE
-extractTime :: String -> Maybe UTCTime
-extractTime line =
-    case splitComma line of
-        (t:_) -> parseTimeStamp t
-        _     -> Nothing
-
--- PARSE CANDLE (same as before)
-parseCandle :: String -> Maybe (UTCTime, Double, Double, Double, Double)
+parseCandle :: String -> Maybe (UTCTime, Double, Double, Double, Double) -- parses the Raw Candle data from C and makes it useable in haskell as doubles
 parseCandle str =
     case splitComma str of
-        [t,o,h,l,c] -> do
+        [t,o,h,l,c] -> do -- 't' Date then time,'o' opening (first trades price), 'h' high  (top wick), 'l' Low (bottom wick), 'c' clsoe 'last trade done that candle'
             time  <- parseTimeStamp t
             open  <- readMaybe o
             high  <- readMaybe h
             low   <- readMaybe l
             close <- readMaybe c
-            return (time, open, high, low, close)
-        _ -> Nothing
+            return (time , open, high, low, close)
 
--- SPLIT CSV
-splitComma :: String -> [String]
+splitComma :: String -> [String]-- takes the new list of items from above and makes them each their own thing
 splitComma s =
     case break (== ',') s of
         (a, ',' : rest) -> a : splitComma rest
         (a, "") -> [a]
 
--- SOCKET SETUP
 resolve :: IO AddrInfo
 resolve = do
     let hints = defaultHints
             { addrFlags = [AI_PASSIVE]
             , addrSocketType = Stream
             }
-    head <$> getAddrInfo (Just hints) Nothing (Just "5002")
+    head <$> getAddrInfo (Just hints) Nothing (Just "5001") -- 5000 wont work on my machine
 
 open :: AddrInfo -> IO Socket
 open addr = do
@@ -135,7 +126,23 @@ open addr = do
     listen sock 1
     return sock
 
--- TIME PARSER
 parseTimeStamp :: String -> Maybe UTCTime
-parseTimeStamp =
-    parseTimeM True defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%S%Q"))
+parseTimeStamp t =
+    parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S" (take 19 t)
+
+loadExistingTimestamps :: FilePath -> IO (Set.Set UTCTime)
+loadExistingTimestamps path = do
+    exists <- doesFileExist path
+    if not exists
+        then return Set.empty
+        else do
+            content <- BS.readFile path  -- STRICT (no lazy issues)
+            let ls = BS.lines content
+                times = mapMaybe (extractTime . BS.unpack) ls
+            return (Set.fromList times)
+
+extractTime :: String -> Maybe UTCTime
+extractTime line =
+    case splitComma line of
+        (t:_) -> parseTimeStamp t
+        _     -> Nothing
